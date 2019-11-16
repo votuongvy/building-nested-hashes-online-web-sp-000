2688115209e2db1736ce09a74476cb9d24291c57
vy vo <tuonggvy.vo@gmail.com>
2019-11-16 05:03:03 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,13 +10,15 @@ def third_challenge
         :age => "54"
         },
       :hero => {
-        #your key/value pairs here
+        :name => "Romeo",
+        :age => "15",
+        :status => "alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        #your key/  here
         },
       :matriarch => {
         #your key/value pairs here
