13ce1df4eabb256f5e6f2f55492baff7e3878895
vy vo <tuonggvy.vo@gmail.com>
2019-11-16 05:03:33 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,7 +18,8 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
-        
+        :name => "Lord Capulet",
+        :age => "50"
         },
       :matriarch => {
         #your key/value pairs here
